25c459ad687f2daff5e9aac8b0bb1f0239636293
liubingjie <breeder1121@hotmail.com>
2017-07-09 16:08:53 +0800

    可以查询我所有的订票

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
-  # self.abstract_class = true
+  self.abstract_class = true
 
-  before_action :authenticate_user_from_token!
+  # before_action :authenticate_user_from_token!
 
   def authenticate_user_from_token!
 
